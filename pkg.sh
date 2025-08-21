4c47b3919dc226b39dbe94dd20b73525d0f921a9
matdombrock <dombrock.mathieu@gmail.com>
2025-08-20 20:43:25 -0700

    update

--- pkg.sh
+++ pkg.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+mkdir -p build
 cd build
 cmake ..
 make -j$(nproc)
@@ -11,8 +12,6 @@ mkdir -p dist
 rm -rf ./dist/*
 cp ./build/mf ./dist/mf
 mkdir -p ./dist/util
-rm -rf ./dist/util/*
 cp ./lua_include/mflib.lua ./dist/util/mflib.lua
 mkdir -p ./dist/examples
-rm -rf ./dist/examples/*
 cp -r ./examples/*.lua ./dist/examples/
