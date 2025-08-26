ee791d1be241b69eb8abcd063cb28efe30ebc79c
matdombrock <dombrock.mathieu@gmail.com>
2025-08-25 23:22:49 -0700

    update

--- src/as.c
+++ src/as.c
@@ -7,7 +7,7 @@
 #include "mf.h"
 #include <portaudio.h>
 
-// Main function handling the audio synthesis callback.
+// Main function handle connecting the audio library to the synthesis engine
 // time_info and user_data are not used here
 static int as_synthesis_callback(const void *input_buffer, void *output_buffer, unsigned long frames_per_buffer,
                                  const PaStreamCallbackTimeInfo *time_info, PaStreamCallbackFlags status_flags,
