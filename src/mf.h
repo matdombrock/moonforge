fcb37e45fc583ed5317ffbf6983fc79cc973a25e
matdombrock <dombrock.mathieu@gmail.com>
2025-08-25 21:19:42 -0700

    update

--- src/mf.h
+++ src/mf.h
@@ -24,7 +24,7 @@ typedef struct {
   float amp;
   float amp_l;
   float amp_r;
-  mfx_lowpass_state lp;
+  mfx_lowpass2_state lp;
   mfx_delay delay;
   enum Wave wave;
 } mf_osc;
@@ -37,9 +37,9 @@ typedef struct {
   mf_osc osc[OSC_COUNT];
   mf_flags flags;
   float bus_amp;
-  mfx_lowpass_state bus_amp_lp;
-  mfx_lowpass_state bus_lp_l;
-  mfx_lowpass_state bus_lp_r;
+  mfx_lowpass2_state bus_amp_lp;
+  mfx_lowpass2_state bus_lp_l;
+  mfx_lowpass2_state bus_lp_r;
   mfx_delay bus_delay_l;
   mfx_delay bus_delay_r;
 } mf_state;
